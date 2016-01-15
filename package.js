6dc3914abccc5207bd7069f9e6fcb40e210a299b
Chris Butler <chris@platformthirteen.com>
2016-01-14 22:14:04 -0500

    update versions

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: "Node-sass is a library that provides binding for Node.js to libsass, the C version of the popular stylesheet preprocessor, Sass.",
-  version: '3.2.0',
+  version: '3.4.2',
   name: "chrisbutler:node-sass",
   git: "https://github.com/chrisbutler/meteor-node-sass"
 });
 
 Npm.depends({
-  'node-sass': "3.2.0",
+  'node-sass': "3.4.2",
 });
 
 Package.onUse(function (api) {
